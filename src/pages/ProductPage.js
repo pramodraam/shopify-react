027525577ac50fdfd337c5dbc58d3a2a5d04f5f5
Pramod Kumar T G <pramodraam@gmail.com>
2024-12-17 10:06:39 +0530

    Cart Functionality

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useContext, use } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import { ShopContext } from '../context/shopContext'
+import { Box, Grid, Image, Text, Button, Heading, Flex, Center } from '@chakra-ui/react'
+
 
 const ProductPage = () => {
 
@@ -14,9 +16,29 @@ const ProductPage = () => {
   if (!product) return <div>Loadings..</div>
 
   return (
-    <div>
-      <h1>{product.title}</h1>
-    </div>
+    <Box>
+      <Grid templateColumns="repeat(2, 1fr)">
+        {product.images && product.images.length > 0 ? (
+        <Image src={product.images[0].src} alt={product.title} />
+        ) : (
+        <Text>No Image Available</Text>
+        )}
+        <Box>
+          <Heading>{product.title}</Heading>
+          {product.variants ? (
+          <Text>${product.variants[0].price.amount}</Text>
+          ) : (
+          <Text>No Variant Available</Text>
+          )}
+          <Text>{product.description}</Text>
+          <Button
+            onClick={()=> addItemToCheckout(product.variants[0].id, 1)}
+          >
+            Add to Cart
+          </Button>
+        </Box>
+      </Grid>
+    </Box>
   )
 }
 
