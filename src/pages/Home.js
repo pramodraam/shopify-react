6b229ab15da016657503036df7ba585988d05367
Pramod Kumar T G <pramodraam@gmail.com>
2024-12-18 13:31:04 +0530

    RichText styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,10 @@ import { Link } from 'react-router-dom'
 import { Box, Grid, Text, Image } from '@chakra-ui/react'
 
 import Hero from '../Components/Hero'
+
 import ImageWithText from '../Components/ImageWithText'
+
+import RichText from '../Components/RichText'
 
 const Home = () => {
 
@@ -22,24 +25,32 @@ const Home = () => {
   return (
     <Box>
       <Hero />
+      <RichText 
+        heading="Relaxation you are waiting for"
+        text="Our bath bombs guarantee a fun"
+      />
       <Grid templateColumns="repeat(3, 1fr)">
         {
           products.map(product => (
             <Link to={`/products/${product.handle}`} key={product.id}>
-              <Box _hover={{ opacity: '80%'}} textAlign="center">
+              <Box _hover={{ opacity: '80%'}} textAlign="center" position="relative">
                 {product.images && product.images.length > 0 ? (
                 <Image src={product.images[0].src} alt={product.title} />
                 ) : (
                 <Text>No Image Available</Text>
                 )}
 
-                <Text>{product.title}</Text>
-                <Text>${product.variants[0].price.amount}</Text>
+                <Text position="absolute" bottom="15%" w="100%" fontWeight="bold">{product.title}</Text>
+                <Text position="absolute" bottom="5%" w="100%" color="gray.500">${product.variants[0].price.amount}</Text>
               </Box>
             </Link>
           ))
         }
       </Grid>
+      <RichText 
+        heading="Treat Yourself"
+        text="Our bath bombs guarantee a fun"
+      />
       <ImageWithText 
       image="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/premium-bath-bombs.jpg?v=1610066758"
       heading="heading"
